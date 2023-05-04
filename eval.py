47bc412f8499cc99fbe1d0d7f6a808c0c24f7f63
rskdh <128662541+rskdh@users.noreply.github.com>
2023-05-04 22:41:50 +0900

    223.05.04 수정
    평가 결과 보기 좋게 출력

--- eval.py
+++ eval.py
@@ -1,3 +1,9 @@
+# eval.py
+'''
+수정 로그
+# 23.05.04 Test 결과 저장 기능들 추가
+'''
+
 from config import *
 from preprocess import *
 from dataLoader import *
@@ -9,6 +15,17 @@ import torch
 from tqdm import tqdm
 import numpy as np
 
+# 사이킷런 #
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import json
+
+import pandas as pd
+# 예측 수행
 def predict(model, test_loader, device):
     
     model.eval()
@@ -23,8 +40,74 @@ def predict(model, test_loader, device):
             model_pred.extend(pred_logit.tolist())
     return model_pred
 
-def eval(test_wav):
 
+# 결과 출력
+def result(df,preds,expInfo=None): # 결과 생성 
+    # 답안지, 정답지 생성
+    df['answer'] = 0
+    df['predict'] = preds
+
+    #파일 이름에서 라벨 추출
+    df.loc[df['file_name'].str.contains('A_01_'), 'answer'] = 0
+    df.loc[df['file_name'].str.contains('A_02_'), 'answer'] = 1
+    df.loc[df['file_name'].str.contains('A_03_'), 'answer'] = 2
+    df.loc[df['file_name'].str.contains('B_01_'), 'answer'] = 3
+    df.loc[df['file_name'].str.contains('B_02_'), 'answer'] = 4
+    df.loc[df['file_name'].str.contains('B_03_'), 'answer'] = 5
+    df['answer'] = df['answer'].astype(int)  # 정답(aswer) colum을 정수형으로 변환
+    
+    answer_arr = df['answer'].tolist()
+    # anslist_arr=np.array(anslist)  # 정답 넘파이 배열
+    # preds_arr=np.array(preds) # 예측 넘파이 배열
+    
+
+    ''' 평가용 결과 출력'''
+
+    """1. Confusion_matrix 출력"""
+    cm = confusion_matrix(answer_arr,preds) # 정답(x축)값, 예측(Y축)값을 이용하여 Confusion Matrix 계산
+    plt.title('Confusion Matrix') # 제목 추가
+    sns.heatmap(cm, annot=True, cmap='Blues') # 도시
+    plt.savefig('%sconfusion_matrix_%s.png'%(result_save, expInfo)) # 그래프를 이미지 파일로 저장
+
+
+    """2. 상세 로그 저장 """
+    df.drop('data',axis=1,inplace=True)  # 소리 데이터 제거
+    df.to_csv("%sTest_evaluation_all_%s.csv" % (result_save, expInfo), index=False) # data frame 
+
+    
+    for i in range(len(preds)):
+        if df['answer'][i]==df['predict'][i]:
+            df=df.drop(i, axis=0)
+    # rs=df.drop_duplicates(subset=['answer', 'predict'], inplace=False) # 오분류 사례만 추출
+    df.to_csv("%sTest_evaluation_error_case_%s.csv"%(result_save, expInfo), index=False) # data frame 
+
+
+    """3. 보고서 생성 및 저장"""
+    # 예측값과 실제값을 이용하여 정확도 계산
+    acc = accuracy_score(answer_arr,preds)
+    acc_report = "%s 모델의 Test 정확도 (합산) : "%model_name+'{:.4f}'.format(acc)
+    print(acc_report)  #결과 출력
+
+    with open("%s/acuracy_report.txt" %result_save , "w") as f:
+        f.write(acc_report)
+
+    # 분류 보고서 생성
+    report = classification_report(answer_arr,preds)
+    print(report)
+ 
+    # 결과를 json으로 저장
+    with open("%s/classification_report_%s.json" %(result_save,expInfo), "w") as outfile:
+        json.dump(report, outfile)
+
+    
+
+
+    
+
+
+
+#모델 평가
+def eval(test_wav):     # Test 데이터 전처리 포함됨.
     test_x = np.array(test_wav.data)
     test_x = set_length(test_x)
     test_X = preprocess_dataset(test_x)
@@ -37,8 +120,85 @@ def eval(test_wav):
     model = CNNclassification().to(device)
     model.load_state_dict(checkpoint)
 
+
     # Inference
     preds = predict(model, test_loader, device)
+    df = test_wav.copy()
+
+    # 답안지, 정답지 생성
+    df['answer'] = 0
+    df['label'] = preds
+    #파일 이름에서 라벨 추출
+    df.loc[df['file_name'].str.contains('A_01_'), 'answer'] = 0
+    df.loc[df['file_name'].str.contains('A_02_'), 'answer'] = 1
+    df.loc[df['file_name'].str.contains('A_03_'), 'answer'] = 2
+    df.loc[df['file_name'].str.contains('B_01_'), 'answer'] = 3
+    df.loc[df['file_name'].str.contains('B_02_'), 'answer'] = 4
+    df.loc[df['file_name'].str.contains('B_03_'), 'answer'] = 5
+    df['answer'] = df['answer'].astype(int)  # 정답 라벨을 정수형으로 변환
+
+    anslist = df['answer'].tolist()
+    anslist_arr=np.array(anslist)  # 정답 넘파이 배열
+    preds_arr=np.array(preds) # 예측 넘파이 배열
+    
+
+    # Confusion_matrix 도시
+    cm = confusion_matrix(anslist_arr,preds_arr)     # 예측값과 실제값을 이용하여 혼동행렬 계산
+    # 히트맵 그리기
+    sns.heatmap(cm, annot=True, cmap='Blues')
+
+# 제목 추가
+    plt.title('Confusion Matrix')
+
+# 그래프를 이미지 파일로 저장
+    plt.savefig('heatmap.jpg')
+    # plt.savefig('confusion_matrix.png')
+
+    # 예측값과 실제값을 이용하여 정확도 계산
+    acc = accuracy_score(anslist_arr,preds_arr)
+    print('모델 전체 정확도: {:.4f}'.format(acc))  # 결과 출력
+
+
+    # 분류 보고서 생성
+    report = classification_report(anslist_arr,preds_arr)
+    print(report)
+
+    # 자료 저장
+    df.drop('data',axis=1,inplace=True)  # 소리 데이터 제거
+    df.to_csv("Test_detail.csv", index=False) # data frame 저장
+
+    
+def evaluation_all(test_wav):
+    test_x = np.array(test_wav.data)
+    test_x = set_length(test_x)
+    test_X = preprocess_dataset(test_x)
+
+    test_data = CustomDataset(X=test_X, y= None, train_mode=False)
+    test_loader = DataLoader(test_data, batch_size = batch_size, shuffle=False)
+
+
+    checkpoint = torch.load(model_save + model_name + '.pth')
+    model = CNNclassification().to(device)
+    model.load_state_dict(checkpoint)
+
+
+    # Inference
+    preds = predict(model, test_loader, device)
+    df = test_wav.copy()
+    del test_wav
+    result(df,preds,expInfo='1')
+
+
+
+
+
+
+
+
+
+
+'''
+preds = predict(model, test_loader, device)
     
     test_wav['label'] = preds
     test_wav = test_wav[['file_name', 'label']]
@@ -65,3 +225,4 @@ def eval(test_wav):
     accuracy = correct / len(pred_df)
     print('correct : %d / %d' % (correct, len(pred_df)))
     print('test accuracy : %.4f' % accuracy)
+'''
--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+# Run.py
+
+'''
+수정 로그
+# 23.05.04 Run 프로그램 실행 모드 선택 기능 추가
+'''
 from dataset import *
 from preprocess import *
 from train import fit
-from eval import eval
+from eval import *
 
 import torch
 import pandas as pd
@@ -22,29 +28,35 @@ def seed_everything(seed):
 
 
 # -------- NOTICE ----------
-# you must modify config.py to suit your environment
+# you must modify config.py to suit your environment // 실행 시키기 전, config.py를 수정하세요.
 
 seed_everything(929)
 
-# load data
-train_wav = train_dataset()
-valid_wav = valid_dataset()
-test_wav = test_dataset()
-
-train_x, train_y = split(train_wav)
-valid_x, valid_y = split(valid_wav)
+''' Run mode Setting'''
+Train = False # 학습 모드
+Test = True # 평가모드
+#Test_set_gen = True # True : 테스트용 학습 데이터를 생성한 후 평가 시행 , False : 
 
 
-# data preprocessing (zero-padding, mfcc)
-train_x = set_length(train_x)
-valid_x = set_length(valid_x)
+if Train == True:
+    # load data
+    train_wav = train_dataset()
+    valid_wav = valid_dataset()
 
-train_X = preprocess_dataset(train_x)
-valid_X = preprocess_dataset(valid_x)
+    train_x, train_y = split(train_wav)
+    valid_x, valid_y = split(valid_wav)
 
+    train_x = set_length(train_x)
+    valid_x = set_length(valid_x)
+    
+    # data preprocessing (zero-padding, mfcc)
+    train_X = preprocess_dataset(train_x)
+    valid_X = preprocess_dataset(valid_x)
+ 
+    # train
+    fit(train_X, train_y, valid_X, valid_y, train_X.shape[2])
 
-# train
-fit(train_X, train_y, valid_X, valid_y, train_X.shape[2])
-
-# evaluation
-eval(test_wav)
+if Test ==True:
+    test_wav = test_dataset()
+    # evaluation
+    evaluation_all(test_wav)
